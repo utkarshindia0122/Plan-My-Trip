cf2508f0ba0afa3a2fc820356e38d8fd9338afdb
Utkarsh <utkarshindia0122@gmail.com>
2022-03-07 20:34:31 +0530

    added rating and award section in restaurants list

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -20,6 +20,20 @@ const PlaceDetails = ({ place }) => {
                 <Typography gutterBottom variant="h5">
                     {place.name}
                 </Typography>
+                <Box display="flex" justifyContent="space-between">
+                    <Typography variant="subtitle1">Prices</Typography>
+                    <Typography gutterBottom variant='subtitle1'>{place.price_level}</Typography>
+                </Box>
+                <Box display="flex" justifyContent="space-between">
+                    <Typography variant="subtitle1">Ranking</Typography>
+                    <Typography gutterBottom variant='subtitle1'>{place.ranking}</Typography>
+                </Box>
+                {place?.awards?.map((award) => (
+                    <Box my={1}display="flex" justifyContent="space-between" alignItems="center">
+                        <img src={award.images.small} alt={award.display_name} />
+                        <Typography variant='subtitle2' color="textSecondary">{award.display_name}</Typography>
+                    </Box>
+                ))}
             </CardContent>
         </Card>
     );
