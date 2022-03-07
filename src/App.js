15f049dab133aef642c7d9a1d875d6816b1bd34f
Utkarsh <utkarshindia0122@gmail.com>
2022-03-07 20:15:54 +0530

    added list of restaurants in list

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ const App = () => {
             <Header />
             <Grid container spacing={3} styles={{ width: '100%' }}>
                 <Grid item xs={12} md={4}>
-                    <List />
+                    <List places={places}/>
 
                 </Grid>
                 <Grid item xs={12} md={8}>
